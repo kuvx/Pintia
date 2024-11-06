7cf2a8dad1bcc2b98d9df6462985e821ac82b460
migudel <miguel.moras@estudiantes.uva.es>
2024-11-06 09:39:33 +0100

    Modificación para agregar dinamismo al footer

--- app/src/main/java/com/example/pintia/components/Footer.kt
+++ app/src/main/java/com/example/pintia/components/Footer.kt
@@ -1,5 +1,6 @@
 package com.example.pintia.components
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
@@ -25,70 +26,54 @@ class Footer @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private val buttons: Array<ImageButton> by lazy {
-        arrayOf(
-            findViewById(R.id.info_button),
-            findViewById(R.id.map_button),
-            findViewById(R.id.home_button),
-            findViewById(R.id.media_button),
-            findViewById(R.id.settings_button)
-        )
-    }
 
-    private fun setActive() {
-        val activeIndex = listOf<String>(
-            InfoActivity::class.java.simpleName,
-            MapActivity::class.java.simpleName,
-            MainMap::class.java.simpleName,
-            GalleryActivity::class.java.simpleName,
-            SettingsActivity::class.java.simpleName
-        ).indexOf(context::class.simpleName)
-        if (activeIndex != -1) updateButtonSizes(activeIndex)
-    }
-
-    fun changeView(index: Int) {
-        val activityClass: Class<out AppCompatActivity> = when (index) {
-            0 -> InfoActivity::class.java
-            1 -> MapActivity::class.java
-            2 -> MainMap::class.java
-            3 -> GalleryActivity::class.java
-            4 -> SettingsActivity::class.java
-            // No debería llegar aquí
-            else -> MainActivity::class.java
-        }
-        val intent = Intent(context, activityClass)
-        context.startActivity(intent)
-        // Ajusta el tamaño del botón seleccionado
-    }
-
-    private fun updateButtonSizes(activeIndex: Int) {
-        // Reestablece el tamaño de todos los botones
-        buttons.forEach { button ->
-            button.layoutParams.width = resources.getDimensionPixelSize(R.dimen.default_button_size)
-            button.layoutParams.height =
-                resources.getDimensionPixelSize(R.dimen.default_button_size)
-            button.requestLayout()
-        }
-
-        // Agranda el botón activo
-        buttons[activeIndex].layoutParams.width =
-            resources.getDimensionPixelSize(R.dimen.active_button_size)
-        buttons[activeIndex].layoutParams.height =
-            resources.getDimensionPixelSize(R.dimen.active_button_size)
-        buttons[activeIndex].setBackgroundResource(R.drawable.round_button_selected_background)
-        buttons[activeIndex].requestLayout()
-    }
+    private val footerEntries: List<Pair<Int, Class<out AppCompatActivity>>> = listOf(
+        Pair(R.drawable.info, InfoActivity::class.java),
+        Pair(R.drawable.map, MapActivity::class.java),
+        Pair(R.drawable.home, MainMap::class.java),
+        Pair(R.drawable.gallery, GalleryActivity::class.java),
+        Pair(R.drawable.settings, SettingsActivity::class.java)
+    )
 
     init {
-
         LayoutInflater.from(context).inflate(R.layout.component_footer, this, true)
 
-        buttons.forEachIndexed { index, button ->
-            button.setOnClickListener {
-                changeView(index)
-            }
-        }
+        val inflater = LayoutInflater.from(context)
+        val linearLayout = findViewById<LinearLayout>(R.id.footer_main_layout)
 
-        setActive()
+        linearLayout.removeAllViews()
+
+        linearLayout.weightSum = footerEntries.size.toFloat()
+
+        footerEntries.forEach { (drawable, activity) ->
+            val item: ImageButton = inflater.inflate(
+                R.layout.component_footer_entry,
+                linearLayout,
+                false
+            ) as ImageButton
+
+            item.setImageResource(drawable)
+            linearLayout.addView(item)
+
+            item.setOnClickListener {
+                val intent = Intent(context, activity)
+                context.startActivity(intent)
+            }
+
+            if (activity.simpleName.equals(context::class.simpleName)) {
+                // Reajuste para indicar que está seleccionado
+                item.layoutParams.width =
+                    resources.getDimensionPixelSize(R.dimen.active_button_size)
+                item.layoutParams.height =
+                    resources.getDimensionPixelSize(R.dimen.active_button_size)
+                item.setBackgroundResource(R.drawable.round_button_selected_background)
+            } else {
+                item.layoutParams.width =
+                    resources.getDimensionPixelSize(R.dimen.default_button_size)
+                item.layoutParams.height =
+                    resources.getDimensionPixelSize(R.dimen.default_button_size)
+            }
+            item.requestLayout()
+        }
     }
 }
