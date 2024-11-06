d2ccb8aa40415af30b0d650732c4a02caa1e2965
migudel <miguel.moras@estudiantes.uva.es>
2024-11-06 11:23:24 +0100

    Funcionalidad de acceso a direcciones web a través de navegador externo

--- app/src/main/java/com/example/pintia/ImageAdapter.kt
+++ app/src/main/java/com/example/pintia/ImageAdapter.kt
@@ -1,16 +1,21 @@
 package com.example.pintia
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 
 class ImageAdapter(private val imageUrls: List<String>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageView)
+        // Asignamos el click listener a cada imagen
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -24,7 +29,23 @@ class ImageAdapter(private val imageUrls: List<String>) : RecyclerView.Adapter<I
             .load(imageUrl)
             .centerCrop()
             .into(holder.imageView)
+        // Asignamos el click listener
+        holder.imageView.setOnClickListener {
+            abrirNavegador(holder.itemView.context, imageUrl) // Llamamos a la función para abrir la URL
+        }
     }
 
     override fun getItemCount() = imageUrls.size
-}
+
+    // Función para abrir el navegador
+    private fun abrirNavegador(context: Context, url: String) {
+        val uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        // Intentar abrir la URL con el navegador predeterminado del dispositivo
+        try {
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(context, "No se pudo abrir la URL", Toast.LENGTH_SHORT).show()
+        }
+    }}
