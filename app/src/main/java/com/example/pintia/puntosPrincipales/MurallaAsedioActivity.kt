ec26a19b6607268875cbf65b57fe04f40bb13436
dancarr <daniel.carracedo@estudiantes.uva.es>
2024-11-18 11:29:16 +0100

    Vista Zona Defensa

--- app/src/main/java/com/example/pintia/puntosPrincipales/MurallaAsedioActivity.kt
+++ app/src/main/java/com/example/pintia/puntosPrincipales/MurallaAsedioActivity.kt
@@ -1,15 +1,33 @@
 package com.example.pintia.puntosPrincipales
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pintia.R
-import com.example.pintia.components.Header
 
 class MurallaAsedioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_muralla_asedio)
-        val header = findViewById<Header>(R.id.header)
+
+        // Configurar el header
+        val header = findViewById<com.example.pintia.components.Header>(R.id.header)
         header.title = getString(R.string.muralla)
+
+        // Referencia al overlay
+        val infoOverlay = findViewById<View>(R.id.info_overlay)
+
+        // Referencia al botón de información
+        val infoButton = findViewById<View>(R.id.info_button)
+
+        // Mostrar el overlay al pulsar el botón de información
+        infoButton.setOnClickListener {
+            infoOverlay.visibility = View.VISIBLE // Muestra el overlay
+        }
+
+        // Ocultar el overlay al hacer clic en cualquier parte del mismo
+        infoOverlay.setOnClickListener {
+            infoOverlay.visibility = View.GONE // Oculta el overlay
+        }
     }
 }
