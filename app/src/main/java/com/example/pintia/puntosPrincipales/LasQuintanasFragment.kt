ff565650ca51ac4308e8655cd18c6d7cde1e431a
diebomb <diego.bombin@estudiantes.uva.es>
2024-12-08 13:45:17 +0100

    quintanas

--- app/src/main/java/com/example/pintia/puntosPrincipales/LasQuintanasFragment.kt
+++ app/src/main/java/com/example/pintia/puntosPrincipales/LasQuintanasFragment.kt
@@ -3,98 +3,59 @@ package com.example.pintia.puntosPrincipales
 import android.os.Bundle
 import com.example.pintia.R
 import com.example.pintia.components.Header
-import android.graphics.PointF
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
-import com.example.pintia.MainActivity
-import com.example.pintia.models.PoligonoColor
-import com.example.pintia.services.OnPoligonoTouchListener
-import com.example.pintia.services.OverlayView
+import com.example.pintia.services.DynamicViewBuilder.pueblaActivity
+import com.example.pintia.services.TTSManager
 
-class LasQuintanasFragment : Fragment(), OnPoligonoTouchListener {
-
+class LasQuintanasFragment : Fragment() {
+    private lateinit var ttsManager: TTSManager
+    private var idOfAudioPlaying = -1
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_las_quintanas, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_info_views, container, false)
+        val context = requireContext()
 
-        requireActivity()
-            .findViewById<Header>(R.id.header)
-            .title = getString(R.string.quinanas)
+        val text = getString(R.string.quinanas)
+        requireActivity().findViewById<Header>(R.id.header)
+            .title = text
+
+        val layout: RelativeLayout = rootView.findViewById(R.id.component_info_views)
+
+        //cambiamos el fondo
+        layout.setBackgroundResource(R.drawable.fondo_quintana)
+        ttsManager = TTSManager(context) { success ->
+            if (success) {
+                // TTS inicializado correctamente
+                ttsManager.setupListener(requireActivity(), idOfAudioPlaying)
+            } else {
+                // Manejar el error de inicialización
+            }
+        }
 
 
-        val overlayView: OverlayView = rootView.findViewById(R.id.overlayView)
+        val path = "quintana"
+        val tituloCod = text.lowercase().replace(" ", "_")
 
-        val poligonos = listOf(
-            PoligonoColor(
-                puntos = listOf(
-                    PointF(224f, 535f),
-                    PointF(580f, 508f),
-                    PointF(722f, 704f),
-                    PointF(224f, 729f)
-                ),
-                title = getString(R.string.sigloIV)
-            ),
-            PoligonoColor(
-                puntos = listOf(
-                    PointF(65f, 753f),
-                    PointF(0f, 1400f),
-                    PointF(891f, 923f),
-                    PointF(722f, 720f)
-                ),
-                title = getString(R.string.sigloI)
-            ),
-            PoligonoColor(
-                puntos = listOf(
-                    PointF(0f, 1430f),
-                    PointF(0f, 1654f),
-                    PointF(1040f, 1654f),
-                    PointF(1040f, 1080f),
-                    PointF(911f, 953f)
-                ),
-                title = getString(R.string.sigloII)
-            ),
-            PoligonoColor(
-                puntos = listOf(
-                    PointF(723f, 635f),
-                    PointF(855f, 597f),
-                    PointF(1040f, 700f),
-                    PointF(1040f, 933F)
-                ),
-                title = getString(R.string.sigloIII)
-            ),
-            PoligonoColor(
-                puntos = listOf(
-                    PointF(224f, 355f),
-                    PointF(580f, 355f),
-                    PointF(660f, 440f),
-                    PointF(224f, 460f)
-                ),
-                title = getString(R.string.pozo)
-            )
-        )
-
-        // Asigna la lista de polígonos a OverlayView para que los dibuje
-        overlayView.poligonos = poligonos
-        // Asigna el listener
-        overlayView.onPoligonoTouchListener = this
+        pueblaActivity(layout, context, path, tituloCod, ttsManager)
 
         return rootView
     }
 
-    // Implementación del listener para cuando se toca un polígono
-    override fun onPoligonoTouched(poligono: PoligonoColor) {
-        Toast.makeText(
-            requireContext(),
-            "Tocaste un polígono: ${poligono.title}",
-            Toast.LENGTH_SHORT
-        ).show()
-        (requireActivity() as MainActivity).changeFrame(poligono.destinationFragment)
+    override fun onPause() {
+        ttsManager.stop()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        ttsManager.shutdown()
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/example/pintia/services/DynamicViewBuilder.kt
+++ app/src/main/java/com/example/pintia/services/DynamicViewBuilder.kt
@@ -8,6 +8,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.graphics.text.LineBreaker
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
